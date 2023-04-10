adcb43fed240bdd2a18ff273408b0c6826eb8e5b
Daryna Panfilova <107915770+Daryna-P@users.noreply.github.com>
2023-04-10 20:37:05 +0300

    video-2 completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000)); 
+
+function onPlay(currentTime){
+    localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+}
+
+const startTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(startTime);
