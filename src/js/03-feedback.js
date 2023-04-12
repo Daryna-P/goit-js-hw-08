1d6e73148221acec501df597ad2ffd22f5f05888
Daryna Panfilova <107915770+Daryna-P@users.noreply.github.com>
2023-04-12 15:21:08 +0300

    Update 03-feedback.js
    final commit to feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,54 @@
 import throttle from 'lodash.throttle';
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('input'),
-    message: document.querySelector('textarea'),
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input'),
+  message: document.querySelector('textarea'),
 };
 const KEY = 'feedback-form-state';
-let formData ={};
+let formData = {};
 populateFormData();
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
 function onFormSubmit(evt) {
-    evt.preventDefault();
-    if (refs.email.value === '' || refs.message.value === ''){
-        alert ('Please fill the form!');
-    }
-    evt.currentTarget.reset(),
-    localStorage.removeItem(KEY),
-    formData = {}
+  evt.preventDefault();
+  if (refs.email.value === '' || refs.message.value === '') {
+    alert('Please fill the form!');
+  } else { evt.currentTarget.reset(), localStorage.removeItem(KEY), (formData = {}); }
+ 
+}
 
-};
 function onFormInput(evt) {
-    formData[evt.target.name] = evt.target.value;
-    localStorage.setItem(KEY, JSON.stringify(formData));
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(KEY, JSON.stringify(formData));
 }
-function populateFormData () {
-    try {
-        const localData = JSON.parse(localStorage.getItem(KEY));
-        if (localData) {
-            if (localData.email) {refs.email.value = localData.email;}
-            
-            if (localData.message) {refs.message.value = localData.message;}
-        }
-    } catch (error) {
-        console.log(error.name);
-        console.log(error.message);
+
+function populateFormData() {
+  const localData = JSON.parse(localStorage.getItem(KEY));
+  if (localData) {
+    if (localData.email) {
+      refs.email.value = localData.email;
     }
+    if (localData.message) {
+      refs.message.value = localData.message;
+    }
+  }
 }
+// function populateFormData () {
+//     try {
+//         const localData = JSON.parse(localStorage.getItem(KEY));
+//         if (localData) {
+//             if (localData.email) {refs.email.value = localData.email;}
+            
+//             if (localData.message) {refs.message.value = localData.message;}
+//         }
+//     } catch (error) {
+//         console.log(error.name);
+//         console.log(error.message);
+//     }
+// }
 
 // if (formData) {
 //     refs.email.value = formData.email;
